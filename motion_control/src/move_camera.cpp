bd72360bb742bf8990c2901d55f4e78357fea022
y.wang <y.wang@synergeticon.de>
2019-04-16 12:26:31 +0200

    update [move_camera] with new wrist angles

--- motion_control/src/move_camera.cpp
+++ motion_control/src/move_camera.cpp
@@ -12,6 +12,8 @@
 #include <moveit_msgs/CollisionObject.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
+float motion_scale = 0.03;
+
 void move_camera ()
 {
   static const std::string PLANNING_GROUP = "camera";
@@ -23,32 +25,32 @@ void move_camera ()
   moveit::core::RobotStatePtr current_state = move_group.getCurrentState ();
   std::vector < double > joint_group_positions;
   current_state->copyJointGroupPositions ( joint_model_group, joint_group_positions );
-  std::cout << "current angle: " << joint_group_positions [ 3 ] << std::endl;
-  // if the start robot pose is pose 2, start move the camera
-  if ( std::abs ( joint_group_positions [ 3 ] + 1.2994 ) <= 0.01 )
+  std::cout << "current wrist 1: " << joint_group_positions [ 3 ] << std::endl;
+  // if the start robot pose is scan_start pose, start move the camera
+  if ( std::abs ( joint_group_positions [ 3 ] + 1.8434 ) <= 0.01 && std::abs ( joint_group_positions [ 5 ] - 3.1415 ) <= 0.01 )
   {
     int motion_stage_idx = 0;
-    while ( motion_stage_idx != 2 )
+    while ( motion_stage_idx != 1 )
     {
       if ( motion_stage_idx  == 0 )
       {
-        // set pose 3 in radians
-        joint_group_positions [ 3 ] = -0.200;
+        // set pose scan_end in radians
+        joint_group_positions [ 3 ] = -0.6;
       } else if ( motion_stage_idx  == 1 )
       {
-        // set pose 2 in radians
-        joint_group_positions [ 3 ] = -1.2994;
+        // set pose scan_start in radians
+        joint_group_positions [ 3 ] = -1.8434;
       }
       move_group.setJointValueTarget ( joint_group_positions );
       moveit::planning_interface::MoveGroupInterface::Plan my_plan;
       bool success = ( move_group.plan ( my_plan ) == moveit::planning_interface::MoveItErrorCode::SUCCESS );
       if ( success )
       {
-        std::cout << "move to: " << joint_group_positions [ 3 ] << std::endl;
+        std::cout << "move wrist 1 to: " << joint_group_positions [ 3 ] << std::endl;
         if ( motion_stage_idx == 0 )
         {
-          move_group.setMaxVelocityScalingFactor ( 0.01 );
-          move_group.setMaxAccelerationScalingFactor ( 0.01 );
+          move_group.setMaxVelocityScalingFactor ( motion_scale );
+          move_group.setMaxAccelerationScalingFactor ( motion_scale );
           move_group.move ();
         }
         if ( motion_stage_idx == 1 )
