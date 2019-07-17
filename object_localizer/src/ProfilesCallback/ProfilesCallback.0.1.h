1fa44b49420e71f6bc6396883368dc6429c03a88
Wang, Yong <wangyongseu9@gmail.com>
2019-07-17 14:20:56 +0200

    add the [profile_merger_c] node which uses library version [1.0]

---
+++ object_localizer/src/ProfilesCallback/ProfilesCallback.0.1.h
@@ -0,0 +1,262 @@
+#ifndef PROFILESCALLBACK_H
+#define PROFILESCALLBACK_H
+
+#include <iostream>
+#include "libllt.h"
+#include <vector>
+
+#define MAX_INTERFACE_COUNT 5
+#define MAX_RESOLUTION 6
+
+// define functions for connecting and disconnecting laser scanner
+void clean_up ( void );
+bool connect_scanner ( std::string serial_number_ );
+bool disconnect_scanner ( void );
+
+void NewProfile ( const void *data, size_t data_size, gpointer user_data );
+void ControlLostCallback ( gpointer user_data );
+
+// laser scanner information
+std::string  serial_number = "218020023";
+LLT *llt_;
+guint32 resolution;
+std::vector<guint8> profile_buffer;
+TScannerType llt_type;
+// event handle
+EHANDLE *event;
+
+// clean up used hLLT and event
+void clean_up ()
+{
+  delete llt_;
+}
+
+// connect laser scanner
+bool connect_scanner ( std::string serial_number_ )
+{
+  // 1. new LLT instance
+  llt_ = new LLT ();
+
+  gint32 ret = 0;
+
+  char *interfaces [ MAX_INTERFACE_COUNT ];
+  guint32 resolutions [ MAX_RESOLUTION ];
+  guint32 interface_count = 0;
+
+  guint32 idle_time = 800;
+  guint32 shutter_time = 200;
+  std::string device_properties_path = "/home/syn/ros_ws/src/microepsilon_scancontrol/scanCONTROL_Linux_SDK_0.1.0";
+
+  // 1. searching for scanCONTROL devices
+  if ( ( ret = GetDeviceInterfaces ( &interfaces[0], MAX_INTERFACE_COUNT ) ) == ERROR_GETDEVINTERFACE_REQUEST_COUNT )
+  {
+    std::cout << "There are more than " << MAX_INTERFACE_COUNT << " scanCONTROL connected" << std::endl;
+    interface_count = MAX_INTERFACE_COUNT;
+  }
+  else if ( ret < 1 )
+  {
+    std::cout << "A error occured during searching for connected scanCONTROL" << std::endl;
+    interface_count = 0;
+  }
+  else
+  {
+    interface_count = ret;
+  }
+
+  // 2. show the number of found scanCONTROLs
+  if ( interface_count == 0 )
+  {
+    std::cout << "There is no scanCONTROL connected - Exiting" << std::endl;
+    clean_up ();
+    return false;
+  }
+  else if ( interface_count == 1 )
+  {
+    std::cout << "There is 1 scanCONTROL connected " << std::endl;
+  }
+  else
+  {
+    std::cout << "There are " << interface_count << " scanCONTROL connected" << std::endl;
+  }
+
+  // 3. show there laser scanners
+  //    and find the target laser scanner with a serial_number
+  bool foundSN = false;
+  int activeDevice = 0;
+  for ( guint32 i = 0; i < interface_count; i++ )
+  {
+    std::cout << interfaces [ i ] << "" << std::endl;
+    std::string tempStr = interfaces [ i ];
+    if ( serial_number_.size () != 0 &&
+         tempStr.compare ( tempStr.size () - serial_number_.size (), serial_number_.size (), serial_number_ ) == 0 )
+    {
+      std::cout << "Found Device with serial number: " << serial_number_ << std::endl;
+      foundSN = true;
+      activeDevice = i;
+      break;
+    }
+  }
+  // if not find the target laser scanner
+  if( !foundSN )
+  {
+    clean_up ();
+    return false;
+  }
+
+  if ( ( ret = SetPathtoDeviceProperties ( device_properties_path ) ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error setting device properties path:\n\t[" << device_properties_path << "]\n";
+    clean_up ();
+    return false;
+  }
+
+  // 5. set device id
+  if ( ( ret = hLLT->SetDeviceInterface ( interfaces[ activeDevice ] ) ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while setting dev id - Error " << ret << "!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 6. connect to the laser scanner
+  if ( ( ret = hLLT->Connect() ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while connecting to the laser scanner - Error " << ret << "!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 7. get the laser scanner information
+  if ( ( ret = hLLT->GetLLTType ( &llt_type ) ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while GetLLTType! - Error " << ret << "!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  if ( ret == GENERAL_FUNCTION_DEVICE_NAME_NOT_SUPPORTED )
+  {
+    std::cout << "Can't decode scanCONTROL type. Please contact Micro Epsilon for a newer version of the library." << std::endl;
+    clean_up ();
+    return false;
+  }
+  // 7.1. print the laser scanner information
+  if ( llt_type >= scanCONTROL27xx_25 && llt_type <= scanCONTROL27xx_xxx )
+  {
+    std::cout << "The scanCONTROL is a scanCONTROL27xx" << std::endl;
+  }
+  else if ( llt_type >= scanCONTROL26xx_25 && llt_type <= scanCONTROL26xx_xxx )
+  {
+    std::cout << "The scanCONTROL is a scanCONTROL26xx" << std::endl;
+  }
+  else if ( llt_type >= scanCONTROL29xx_25 && llt_type <= scanCONTROL29xx_xxx )
+  {
+    std::cout << "The scanCONTROL is a scanCONTROL29xx" << std::endl;
+  }
+  else
+  {
+    std::cout << "The scanCONTROL is a undefined type\nPlease contact Micro-Epsilon for a newer SDK" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 8. get all possible resolutions of the selected laser scanner
+  std::cout << "Get all possible resolutions" << std::endl;
+  if ( ( ret = hLLT->GetResolutions ( &resolutions [ 0 ], MAX_RESOLUTION ) ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error GetResolutions!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 8.1. set resolution to the max possible resolution
+  resolution = resolutions [ 0 ];
+  if ( hLLT->SetResolution ( resolution ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while setting resolution!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 9. set profile
+  if ( hLLT->SetProfileConfig ( PROFILE ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while setting SetProfileConfig!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 10. set idle_time
+  if ( hLLT->SetFeature ( FEATURE_FUNCTION_IDLETIME, idle_time ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while setting FEATURE_FUNCTION_IDLETIME!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 10. set shutter_time
+  if ( hLLT->SetFeature ( FEATURE_FUNCTION_SHUTTERTIME, shutter_time ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while setting FEATURE_FUNCTION_SHUTTERTIME!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 10. set TRIG_INTERNAL
+  if ( hLLT->SetFeature ( FEATURE_FUNCTION_TRIGGER, TRIG_INTERNAL ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while setting FEATURE_FUNCTION_TRIGGER!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  // 11. register Callbacks for program handling
+  std::cout << "Register callbacks" << std::endl;
+  if ( hLLT->RegisterBufferCallback ( (gpointer)&NewProfile, NULL ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while registering buffer callback!" << std::endl;
+    clean_up ();
+    return false;
+  }
+
+  if ( hLLT->RegisterControlLostCallback( (gpointer)&ControlLostCallback, NULL ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while registering control lost callback!" << std::endl;
+    clean_up ();
+    return false;
+  }
+  return true;
+}
+
+// disconnect laser scanner
+bool disconnect_scanner ()
+{
+  gint32 ret = 0;
+  std::cout << "Disconnecting..." << std::endl;
+  if ( ( ret = hLLT->Disconnect() ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while disconnecting - Error " << ret << "!" << std::endl;
+  }
+  clean_up();
+  return true;
+}
+
+// save new profile into profile_buffer
+void NewProfile ( const void *data, size_t data_size, gpointer user_data )
+{
+  if ( data != NULL && data_size == profile_buffer.size() )
+  {
+    memcpy ( &profile_buffer[0], data, data_size );
+    set_event ( event );
+  }
+}
+
+// control of the device is lost. Display a message and reconnect!
+void ControlLostCallback ( gpointer user_data )
+{
+  std::cout << "Control lost" << std::endl;
+  disconnect_scanner ();
+  connect_scanner ( serial_number );
+}
+
+#endif // PROFILESCALLBACK_H
---
+++ object_localizer/src/profile_merger_c_thread.0.1.cpp
@@ -0,0 +1,353 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <thread>
+#include <mutex>
+
+#include <ros/ros.h>
+#include <std_srvs/Empty.h>
+#include <ros/package.h>
+#include <ctime>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf/transform_listener.h>
+
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+
+#include "ProfilesCallback/ProfilesCallback.0.1.h"
+#include "ProfilesCallback/ThreadSafeQueue.h"
+
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud< PointT > PointCloudT;
+std::string reference_frame = "world";
+std::string scanner_frame = "scanCONTROL_2900-50_scanner_laser_link";
+double lag_compensation_ = 0.001;
+static const int num_threads = 1;
+bool is_stop = false;
+bool is_publish_ = false;
+int current_thread_index = 0;
+ThreadSafeQueue < PointCloudT::Ptr > profile_thread_queue [ num_threads ];
+ThreadSafeQueue < PointCloudT::Ptr > scene_pc_queue;
+
+// filtering point cloud
+void filterOutliner ( PointCloudT::Ptr cloud )
+{
+	static pcl::StatisticalOutlierRemoval < PointT > sor;
+	sor.setStddevMulThresh ( 1.0 );
+  sor.setInputCloud ( cloud );
+  sor.setMeanK ( cloud->size() / 2 );
+  sor.filter ( *cloud );
+}
+
+void downSampling ( PointCloudT::Ptr cloud, PointCloudT::Ptr cloud_sampled )
+{
+  static pcl::VoxelGrid < PointT > grid;
+  grid.setInputCloud ( cloud );
+  grid.setLeafSize ( 0.0002f, 0.0002f, 0.0002f );
+  grid.filter ( *cloud_sampled );
+	std::printf ( "Downsampled cloud size is [%d, %d]\n", cloud_sampled->width, cloud_sampled->height );
+}
+
+class ProfileMerger
+{
+public:
+	void transform_point_cloud ( PointCloudT::Ptr cloud, PointCloudT::Ptr cloud_out )
+	{
+	  tf::StampedTransform transform;
+		ros::Time sample_time = pcl_conversions::fromPCL ( cloud->header.stamp );
+		bool is_lookuped = false;
+		int delay_counter = 0;
+		while ( is_lookuped == false )
+		{
+			try
+			{
+		    listener.lookupTransform ( reference_frame, scanner_frame, sample_time, transform );
+				is_lookuped = true;
+		  }
+		  catch ( tf::TransformException ex )
+		  {
+				if ( boost::starts_with ( ex.what(), "Lookup would require extrapolation into the future." ) )
+				{
+					// wait for 0.01 second every time
+					ros::Duration ( 0.01 ).sleep ();
+					if ( delay_counter == 4 )
+					{
+						return;
+					}
+					delay_counter++;
+				}
+				else
+				{
+					ROS_ERROR ( "%s", ex.what() );
+					return;
+				}
+		  }
+		}
+
+		tf::Vector3 point ( 0, 0, 0 );
+		tf::Vector3 point_n ( 0, 0, 0 );
+		for ( PointT temp_point : cloud->points )
+		{
+			point.setX ( temp_point.x );
+			point.setY ( temp_point.y );
+			point.setZ ( temp_point.z );
+			if ( temp_point.y < 0.085 )
+      {
+        continue;
+      }
+			tf::Vector3 point_n = transform * point;
+			temp_point.x = point_n.getX ();
+			temp_point.y = point_n.getY ();
+			temp_point.z = point_n.getZ ();
+			cloud_out->points.push_back ( temp_point );
+		}
+	}
+
+  void transform_cb ( int thread_id )
+  {
+		std::cout << "Transform thread [" << thread_id << "] is started" << std::endl;
+		while ( !is_stop )
+		{
+			if ( !is_publish_ )
+			{
+				ros::Duration ( 0.01 * num_threads ).sleep ();
+				continue;
+			}
+
+			// get the front profile
+			PointCloudT::Ptr in_cloud ( new PointCloudT );
+			profile_thread_queue [ thread_id ].pop ( in_cloud );
+			if ( in_cloud->size() == 0 )
+			{
+				continue;
+			}
+
+			// transforming the input point cloud
+			PointCloudT::Ptr in_cloud_transformed	( new PointCloudT );
+			transform_point_cloud ( in_cloud, in_cloud_transformed );
+			std::cout << "Transform thread [" << thread_id << "]: input point cloud after transforming has [" << in_cloud_transformed->size() << "] data points" << std::endl;
+			if ( in_cloud_transformed->size() == 0 )
+			{
+				continue;
+			}
+
+			scene_pc_queue.push ( in_cloud_transformed );
+		}
+
+		std::cout << "Transform thread [" << thread_id << "] is stopped" << std::endl;
+  }
+
+	void merger_cb ()
+	{
+		std::cout << "Merger thread is started" << std::endl;
+		while ( !is_stop )
+		{
+			if ( scene_pc_queue.isEmpty () )
+			{
+				ros::Duration ( 0.01 * num_threads ).sleep ();
+				continue;
+			}
+
+			// get the front profile
+			PointCloudT::Ptr in_cloud ( new PointCloudT );
+			scene_pc_queue.pop ( in_cloud );
+			if ( in_cloud->size() == 0 )
+			{
+				return;
+			}
+
+			// merge input point cloud with scene point cloud
+			*scene_cloud += *in_cloud;
+			PointCloudT::Ptr scene_cloud_sampled	( new PointCloudT );
+			downSampling ( scene_cloud, scene_cloud_sampled );
+			scene_cloud = scene_cloud_sampled;
+			std::cout << "Scene point cloud has [" << scene_cloud->size() << "] data points" << std::endl;
+
+			// show the scene point cloud
+			scene_cloud->header.frame_id = reference_frame;
+			pcl_conversions::toPCL ( ros::Time::now(), scene_cloud->header.stamp );
+			cloud_pub_.publish ( scene_cloud );
+		}
+		std::cout << "Merger thread is stopped" << std::endl;
+	}
+
+	bool start_profile_merger ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
+	{
+		// clear the scene point cloud
+		is_publish_ = false;
+		ros::Duration ( 0.01 * ( num_threads + 1 ) ).sleep ();
+		scene_cloud->clear ();
+		is_publish_ = true;
+	  return true;
+	}
+
+	bool stop_profile_merger ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
+	{
+	  is_publish_ = false;
+		while ( ! scene_pc_queue.isEmpty () )
+		{
+			ros::Duration ( 0.04 * num_threads ).sleep ();
+			continue;
+		}
+	  return true;
+	}
+
+  ProfileMerger () : scene_cloud ( new pcl::PointCloud< PointT > )
+  {
+		is_publish_ = false;
+		start_profile_merger_ = nh_.advertiseService ( "start_profile_merger", &ProfileMerger::start_profile_merger, this );
+		stop_profile_merger_ = nh_.advertiseService ( "stop_profile_merger", &ProfileMerger::stop_profile_merger, this );
+		ros::Duration ( 1 ).sleep ();
+
+    std::string cloud_out_name = "/profile_merger/points";
+    cloud_pub_ = nh_.advertise < PointCloudT > ( cloud_out_name, 30 );
+    ROS_INFO_STREAM ( "Publishing point cloud message on topic " << cloud_out_name );
+  }
+
+  ~ProfileMerger () { }
+
+private:
+  ros::NodeHandle nh_;
+	tf::TransformListener listener;
+	ros::ServiceServer start_profile_merger_, stop_profile_merger_;
+	pcl::PointCloud<PointT>::Ptr scene_cloud;
+  ros::Publisher cloud_pub_;
+};
+
+double average ( double a, double b )
+{
+	return ( a + b ) / 100000.0 / 2.0;
+}
+
+void scanner_cb ()
+{
+  gint32 ret = 0;
+
+  // 1. connect the laser scanner
+  if ( ! connect_scanner ( serial_number ) )
+  {
+		std::cout << "Error in connecting the laser scanner!" << std::endl;
+    return;
+  }
+
+  std::vector < double > value_x, value_z;
+  profile_buffer.resize ( resolution * 64 );
+  value_x.resize ( resolution );
+  value_z.resize ( resolution );
+  CInterfaceLLT::ResetEvent ( event );
+
+  // 2. start transfer profoles
+  if ( ( ret = hLLT->TransferProfiles ( NORMAL_TRANSFER, true ) ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error in profile transfer! - [" << ret << "]" << std::endl;
+    return;
+  }
+
+  // 3. loop until receive the end signal
+  while ( !is_stop )
+  {
+    if ( CInterfaceLLT::WaitForSingleObject ( event, 2000 ) != WAIT_OBJECT_0 )
+    {
+      continue;
+    }
+    CInterfaceLLT::ResetEvent ( event );
+
+		if ( !is_publish_ )
+		{
+			ros::Duration ( 0.01 * num_threads ).sleep ();
+			continue;
+		}
+
+		// 3.1. get x and z arrays
+    if ( ( ret = ConvertProfile2Values ( &profile_buffer [ 0 ], profile_buffer.size(), & ( hLLT->appData ), resolution, PROFILE, 0, NULL, NULL, NULL, &value_x [ 0 ], &value_z [ 0 ], NULL, NULL ) ) != ( CONVERT_X | CONVERT_Z ) )
+    {
+      std::cout << "Error while extracting profiles! - [" << ret << "]" << std::endl;
+      continue;
+    }
+
+    // 3.2. get profile_counter and time information
+    guint32 profile_counter = 0;
+    double shutter_closed = 0, shutter_opened = 0;
+    Timestamp2TimeAndCount ( &profile_buffer[0], &shutter_opened, &shutter_closed, &profile_counter );
+		std::cout << "Profile: [" << profile_counter << "] has time [shutter_closed, shutter_opened] = [" << shutter_closed << "," << shutter_opened << "]" << std::endl;
+
+    // 3.3. create a new profile point cloud
+    PointCloudT::Ptr profile_cloud ( new PointCloudT );
+    profile_cloud->header.frame_id = scanner_frame;
+    ros::Time profile_time = ros::Time::now() - ros::Duration ( average ( shutter_opened, shutter_closed ) + lag_compensation_ );
+    pcl_conversions::toPCL ( profile_time, profile_cloud->header.stamp );
+    PointT temp_point;
+    for ( int i = 0; i < value_x.size (); ++i )
+    {
+      if ( value_z [ i ] > 30 )
+      {
+        temp_point.x = - value_x [ i ] / 1000.0;
+        temp_point.y = value_z [ i ] / 1000.0;
+        temp_point.z = 0.0;
+        profile_cloud->points.push_back ( temp_point );
+      }
+    }
+
+		if ( profile_cloud->size () == 0 )
+		{
+			continue;
+		}
+
+		// 3.4. put the new profile point cloud into each profile_thread_queue
+		std::cout << "Profile [" << profile_counter << "] for thread [" << current_thread_index << "]" << std::endl;
+		profile_thread_queue [ current_thread_index ].push ( profile_cloud );
+		current_thread_index = ( current_thread_index + 1 ) % num_threads;
+  }
+
+  // 4. stop transfer profiles
+  if ( ( ret = hLLT->TransferProfiles ( NORMAL_TRANSFER, false ) ) < GENERAL_FUNCTION_OK )
+  {
+    std::cout << "Error while stopping transmission! - [" << ret << "]" << std::endl;
+  }
+
+  // 5. disconnect the laser scanner
+  disconnect_scanner ();
+
+	std::cout << "scanner thread is stopped!" << std::endl;
+}
+
+int main ( int argc, char** argv )
+{
+	ros::init ( argc, argv, "profile_merger_c_thread" );
+	ros::AsyncSpinner spinner ( 3 );
+  spinner.start ();
+	ProfileMerger pm;
+
+	// create a merger thread, [num_threads] transform threads, and a scanner thread
+	std::thread merger_thread =  std::thread ( &ProfileMerger::merger_cb, &pm );
+	std::thread transform_thread [ num_threads ];
+  for ( int idx = 0; idx < num_threads; ++idx )
+  {
+    transform_thread [ idx ] = std::thread ( &ProfileMerger::transform_cb, &pm, idx );
+  }
+	std::thread scanner_thread =  std::thread ( &scanner_cb );
+
+	// wait for stop signal
+  ros::waitForShutdown ();
+	is_stop = true;
+
+	// Join the threads with the main thread
+	scanner_thread.join ();
+  for ( int idx = 0; idx < num_threads; ++idx )
+  {
+    transform_thread [ idx ].join ();
+  }
+	merger_thread.join ();
+
+  return 0;
+}
