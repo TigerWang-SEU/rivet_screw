69b634ec366baea94140e6554e8bb211fff33209
Wang, Yong <wangyongseu9@gmail.com>
2019-10-21 12:10:57 +0200

    remove a variable [scene_cloud_] and update functions [transform_point_cloud, cloud_cb]

--- object_localizer/src/pcl_merger.cpp
+++ object_localizer/src/pcl_merger.cpp
@@ -21,58 +21,56 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/features/normal_3d.h>
 
+#include "head/reference_frame.h"
+
 typedef pcl::PointXYZRGB PointT;
 typedef pcl::PointCloud< PointT > PointCloudT;
-std::string reference_frame = "world";
-std::string camera_frame = "camera_color_optical_frame";
-ros::Time sample_time;
 
 void downSampling ( PointCloudT::Ptr cloud, PointCloudT::Ptr cloud_sampled )
 {
   static pcl::VoxelGrid<pcl::PointXYZRGB> grid;
   grid.setInputCloud ( cloud );
-  grid.setLeafSize ( 0.005f, 0.005f, 0.01f );
+  grid.setLeafSize ( 0.008f, 0.008f, 0.008f );
   grid.filter ( *cloud_sampled );
-	std::printf( "Downsampled cloud size is %d, %d\n", cloud_sampled->width, cloud_sampled->height );
+  std::printf ( "Downsampled cloud size is %lu\n", cloud_sampled->size() );
 }
 
 class PointCloudMerger
 {
 public:
+  void transform_point_cloud ( PointCloudT::Ptr cloud, PointCloudT::Ptr cloud_out, ros::Time& sample_time)
+  {
+    tf::StampedTransform transform;
+    bool is_lookuped = false;
+    while ( is_lookuped == false )
+    {
+      try
+      {
+        listener.lookupTransform ( reference_frame, camera_frame, sample_time, transform );
+        is_lookuped = true;
+      }
+      catch ( tf::TransformException ex )
+      {
+        if ( boost::starts_with ( ex.what(), "Lookup would require extrapolation into the future." ) )
+        {
+          ros::Duration ( 0.05 ) .sleep ();
+        }
+        else
+        {
+          ROS_ERROR ( "%s", ex.what() );
+          return;
+        }
+      }
+    }
 
-	void transform_point_cloud ( PointCloudT::Ptr cloud, PointCloudT::Ptr cloud_out )
-	{
-	  tf::StampedTransform transform;
-		bool is_lookuped = false;
-		while ( is_lookuped == false )
-		{
-			try
-			{
-		    listener.lookupTransform( reference_frame, camera_frame, sample_time, transform );
-				is_lookuped = true;
-		  }
-		  catch ( tf::TransformException ex )
-		  {
-				if ( boost::starts_with ( ex.what(), "Lookup would require extrapolation into the future." ) )
-				{
-					// wait for 0.05 second every time
-					ros::Duration ( 0.05 ) .sleep ();
-				} else
-				{
-					ROS_ERROR ( "%s", ex.what() );
-					return;
-				}
-		  }
-		}
-
-		tf::Vector3 point(0, 0, 0);
-		tf::Vector3 point_n(0, 0, 0);
-		for ( PointT temp_point : cloud->points )
-		{
-			point.setX( temp_point.x );
-			point.setY( temp_point.y );
-			point.setZ( temp_point.z );
-      if( !pcl_isfinite ( temp_point.x ) )
+    tf::Vector3 point_o ( 0, 0, 0 );
+    tf::Vector3 point_n ( 0, 0, 0 );
+    for ( PointT temp_point : cloud->points )
+    {
+      point_o.setX ( temp_point.x );
+      point_o.setY ( temp_point.y );
+      point_o.setZ ( temp_point.z );
+      if ( !pcl_isfinite ( temp_point.x ) )
       {
         continue;
       }
@@ -80,55 +78,47 @@ public:
       {
         continue;
       }
-			tf::Vector3 point_n = transform * point;
-			temp_point.x = point_n.getX();
-			temp_point.y = point_n.getY();
-			temp_point.z = point_n.getZ();
+      tf::Vector3 point_n = transform * point_o;
+      temp_point.x = point_n.getX ();
+      temp_point.y = point_n.getY ();
+      temp_point.z = point_n.getZ ();
       if ( temp_point.z < 1.49 || temp_point.z > 1.82 )
       {
         continue;
       }
-			cloud_out->points.push_back ( temp_point );
-		}
-	}
+      cloud_out->points.push_back ( temp_point );
+    }
+  }
 
   void cloud_cb ( const sensor_msgs::PointCloud2::ConstPtr& cloud )
   {
-    // if don't merge or input cloud is not dense or is empty, publish the old point cloud and return
-    // !cloud->is_dense the is dense doesn't work any more.
     if ( !is_merge_ || ( cloud->width * cloud->height ) == 0 )
-		{
+    {
       scene_cloud_total->header.frame_id = reference_frame;
-			pcl_conversions::toPCL ( ros::Time::now(), scene_cloud_total->header.stamp );
-	    cloud_pub_.publish ( scene_cloud_total );
-			return;
-		}
+      pcl_conversions::toPCL ( ros::Time::now(), scene_cloud_total->header.stamp );
+      cloud_pub_.publish ( scene_cloud_total );
+      return;
+    }
 
-    // convert input ros cloud to pcl cloud
-    // and save it in local variable scene_cloud
     pcl::PCLPointCloud2 pcl_pc2;
     pcl_conversions::toPCL( *cloud, pcl_pc2 );
+    PointCloudT::Ptr scene_cloud_ ( new PointCloudT );
     pcl::fromPCLPointCloud2 ( pcl_pc2, *scene_cloud_ );
-		sample_time = cloud->header.stamp;
-		std::cout << "[" << sample_time << "] Input point cloud has [" << scene_cloud_->width << "*" << scene_cloud_->height << " = " << scene_cloud_->width * scene_cloud_->height << "] data points" << std::endl;
+    ros::Time sample_time = cloud->header.stamp;
+    std::cout << "[" << sample_time << "] Input cloud has [" << scene_cloud_->size () << "] data points." << std::endl;
 
-		// transforming the input point cloud
-		PointCloudT::Ptr scene_cloud_sampled	(new PointCloudT);
-    *scene_cloud_sampled += *scene_cloud_;
-		PointCloudT::Ptr scene_cloud_world	(new PointCloudT);
-    scene_cloud_world->header.frame_id = reference_frame;
-		transform_point_cloud ( scene_cloud_sampled, scene_cloud_world );
-    pcl_conversions::toPCL ( ros::Time::now(), scene_cloud_world->header.stamp );
-    // cloud_pub_.publish ( scene_cloud_world );
-		std::cout << "***scene_cloud_world has [" << scene_cloud_world->size() << "] data points" << std::endl;
+    // transforming the input cloud
+    PointCloudT::Ptr scene_cloud_world	( new PointCloudT );
+    transform_point_cloud ( scene_cloud_, scene_cloud_world, sample_time );
+    std::cout << "scene_cloud_world has [" << scene_cloud_world->size() << "] data points" << std::endl;
 
-		// merging the input point cloud
-		*scene_cloud_total += *scene_cloud_world;
-		std::cout << "@@@scene_cloud_total has [" << scene_cloud_total->size() << "] data points" << std::endl;
-		PointCloudT::Ptr scene_cloud_total_temp	( new PointCloudT );
-		downSampling ( scene_cloud_total, scene_cloud_total_temp );
-		std::cout << "###scene_cloud_total_temp has [" << scene_cloud_total_temp->size() << "] data points" << std::endl;
-		scene_cloud_total = scene_cloud_total_temp;
+    // merging the input point cloud
+    *scene_cloud_total += *scene_cloud_world;
+    std::cout << "scene_cloud_total has [" << scene_cloud_total->size() << "] data points" << std::endl;
+    PointCloudT::Ptr scene_cloud_total_temp ( new PointCloudT );
+    downSampling ( scene_cloud_total, scene_cloud_total_temp );
+    std::cout << "scene_cloud_total_temp has [" << scene_cloud_total_temp->size() << "] data points" << std::endl;
+    scene_cloud_total = scene_cloud_total_temp;
     scene_cloud_total->header.frame_id = reference_frame;
     pcl_conversions::toPCL ( ros::Time::now(), scene_cloud_total->header.stamp );
     cloud_pub_.publish ( scene_cloud_total );
@@ -136,12 +126,7 @@ public:
 
   bool start_pcl_merge ( std_srvs::Empty::Request& req, std_srvs::Empty::Response& res )
   {
-    // clear old merger point cloud
     scene_cloud_total->clear ();
-    scene_cloud_total->header.frame_id = reference_frame;
-    pcl_conversions::toPCL ( ros::Time::now(), scene_cloud_total->header.stamp );
-    cloud_pub_.publish ( scene_cloud_total );
-    // then start to merge new point cloud
     is_merge_ = true;
     return true;
   }
@@ -152,12 +137,11 @@ public:
     return true;
   }
 
-  PointCloudMerger () : scene_cloud_ ( new pcl::PointCloud< PointT > ), scene_cloud_total ( new pcl::PointCloud< PointT > )
+  PointCloudMerger () : scene_cloud_total ( new pcl::PointCloud< PointT > )
   {
     is_merge_ = false;
     start_pcl_merge_ = nh_.advertiseService ( "start_pcl_merge", &PointCloudMerger::start_pcl_merge, this );
     end_pcl_merge_ = nh_.advertiseService ( "stop_pcl_merge", &PointCloudMerger::end_pcl_merge, this );
-    ros::Duration ( 1.0 ).sleep ();
 
     std::string cloud_topic_in_ = "/camera/depth_registered/points";
     cloud_sub_ = nh_.subscribe ( cloud_topic_in_, 200, &PointCloudMerger::cloud_cb, this );
@@ -168,13 +152,12 @@ public:
     ROS_INFO_STREAM ( "Publishing point cloud message on topic " << cloud_topic_out_ );
   }
 
-  ~PointCloudMerger () { }
+  ~PointCloudMerger () {}
 
 private:
   ros::NodeHandle nh_;
-	tf::TransformListener listener;
-  pcl::PointCloud<PointT>::Ptr scene_cloud_;
-	pcl::PointCloud<PointT>::Ptr scene_cloud_total;
+  tf::TransformListener listener;
+  pcl::PointCloud<PointT>::Ptr scene_cloud_total;
   bool is_merge_;
   ros::ServiceServer start_pcl_merge_, end_pcl_merge_;
   ros::Subscriber cloud_sub_;
